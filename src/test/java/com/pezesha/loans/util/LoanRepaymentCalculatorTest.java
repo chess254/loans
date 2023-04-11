f6cb5788e8981b08258778e41d8a18f196df0902
Caleb Achesa <lae2006a@yahoo.com>
2023-04-11 05:13:30 +0300

    [FIX] updated readme.md, format table in index.html and loanform.html

--- src/test/java/com/pezesha/loans/util/LoanRepaymentCalculatorTest.java
+++ src/test/java/com/pezesha/loans/util/LoanRepaymentCalculatorTest.java
@@ -1,32 +1,31 @@
-//package com.pezesha.loans.util;
-//
-//import org.junit.jupiter.api.Disabled;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//class LoanRepaymentCalculatorTest {
+package com.pezesha.loans.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LoanRepaymentCalculatorTest {
 //    @Disabled
-//    @Test
-//    public void testCalculateRepaymentSchedule() {
-//        LoanRepaymentCalculator calculator = new LoanRepaymentCalculator();
-//
-//        // Test case 1
-//        double loanAmount1 = 100000;
-//        int loanTermMonths1 = 10;
-//        double interestRate1 = 5.5;
-//        RepaymentFrequency repaymentFrequency1 = RepaymentFrequency.MONTHLY;
-//
-//        LoanRepaymentSchedule schedule1 = calculator.calculateRepaymentSchedule(
-//                loanAmount1, loanTermMonths1, interestRate1, repaymentFrequency1);
-//
-//
-//        assertEquals(10253.81, schedule1.getRepayments().get(0).getPaymentAmount(), 0.01);
-//        assertEquals(2538.13, schedule1.getTotalInterest(), 0.01);
-//        assertEquals(0, schedule1.getRepayments().get(9).getBalance(), 0.01);
-//        assertEquals(90204.52, schedule1.getRepayments().get(0).getBalance(), 0.01);
-//
-//
-//
-//    }
-//}
+    @Test
+    public void testCalculateRepaymentSchedule() {
+        LoanRepaymentCalculator calculator = new LoanRepaymentCalculator();
+
+        // Test case 1
+        double loanAmount1 = 100000;
+        int loanTermMonths1 = 10;
+        double interestRate1 = 5.5;
+        RepaymentFrequency repaymentFrequency1 = RepaymentFrequency.MONTHLY;
+
+        LoanRepaymentSchedule schedule1 = calculator.calculateRepaymentSchedule(
+                loanAmount1, loanTermMonths1, interestRate1, repaymentFrequency1);
+
+
+        assertEquals(10253.81, schedule1.getRepayments().get(0).getPaymentAmount(), 0.01);
+        assertEquals(2538.13, schedule1.getTotalInterest(), 0.01);
+        assertEquals(0, schedule1.getRepayments().get(9).getBalance(), 0.01);
+        assertEquals(90204.52, schedule1.getRepayments().get(0).getBalance(), 0.01);
+
+
+
+    }
+}
--- src/test/java/com/pezesha/loans/util/LoanRepaymentScheduleTest.java
+++ src/test/java/com/pezesha/loans/util/LoanRepaymentScheduleTest.java
@@ -1,53 +1,53 @@
-//package com.pezesha.loans.util;
-//
-//
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Disabled;
-//import org.junit.jupiter.api.Test;
-//
-//import java.util.List;
-//@Disabled
-//public class LoanRepaymentScheduleTest {
-//
-//    @Test
-//    public void testLoanRepaymentSchedule() {
-//        double loanAmount = 10000.00;
-//        int loanTermMonths = 12;
-//        double interestRate = 5.0;
-//        RepaymentFrequency repaymentFrequency = RepaymentFrequency.MONTHLY;
-//
-//        LoanRepaymentSchedule schedule = new LoanRepaymentSchedule(loanAmount, loanTermMonths, interestRate, repaymentFrequency);
-//
-//        // Add payments
-//        double monthlyInterestRate = interestRate / 100 / repaymentFrequency.getNumPaymentsPerYear();
-//        double paymentAmount = loanAmount * monthlyInterestRate / (1 - Math.pow(1 + monthlyInterestRate, -loanTermMonths));
-//        double balance = loanAmount;
-//
-//        for (int month = 1; month <= loanTermMonths; month++) {
-//            double interest = balance * monthlyInterestRate;
-//            double principal = paymentAmount - interest;
-//            balance -= principal;
-//
-//            schedule.addPayment(month, paymentAmount, principal, interest, balance);
-//        }
-//
-//        // Test results
-//        List<LoanRepayment> repayments = schedule.getRepayments();
-//
-//        Assertions.assertEquals(12, repayments.size());
-//
-//        Assertions.assertEquals(856.07, repayments.get(0).getPaymentAmount(), 0.01);
-//        Assertions.assertEquals(451.97, repayments.get(0).getPrincipal(), 0.01);
-//        Assertions.assertEquals(52.08, repayments.get(0).getInterest(), 0.01);
-//        Assertions.assertEquals(9548.03, repayments.get(0).getBalance(), 0.01);
-//
-//        Assertions.assertEquals(1044.02, repayments.get(11).getPaymentAmount(), 0.01);
-//        Assertions.assertEquals(469.82, repayments.get(11).getPrincipal(), 0.01);
-//        Assertions.assertEquals(33.23, repayments.get(11).getInterest(), 0.01);
-//        Assertions.assertEquals(0.00, repayments.get(11).getBalance(), 0.01);
-//
-//        Assertions.assertEquals(12528.22, schedule.getTotalPaymentAmount(), 0.01);
-//        Assertions.assertEquals(10000.00, schedule.getTotalPrincipal(), 0.01);
-//        Assertions.assertEquals(2528.22, schedule.getTotalInterest(), 0.01);
-//    }
-//}
+package com.pezesha.loans.util;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+@Disabled
+public class LoanRepaymentScheduleTest {
+
+    @Test
+    public void testLoanRepaymentSchedule() {
+        double loanAmount = 10000.00;
+        int loanTermMonths = 12;
+        double interestRate = 5.0;
+        RepaymentFrequency repaymentFrequency = RepaymentFrequency.MONTHLY;
+
+        LoanRepaymentSchedule schedule = new LoanRepaymentSchedule(loanAmount, loanTermMonths, interestRate, repaymentFrequency);
+
+        // Add payments
+        double monthlyInterestRate = interestRate / 100 / repaymentFrequency.getNumPaymentsPerYear();
+        double paymentAmount = loanAmount * monthlyInterestRate / (1 - Math.pow(1 + monthlyInterestRate, -loanTermMonths));
+        double balance = loanAmount;
+
+        for (int month = 1; month <= loanTermMonths; month++) {
+            double interest = balance * monthlyInterestRate;
+            double principal = paymentAmount - interest;
+            balance -= principal;
+
+            schedule.addPayment(month, paymentAmount, principal, interest, balance);
+        }
+
+        // Test results
+        List<LoanRepayment> repayments = schedule.getRepayments();
+
+        Assertions.assertEquals(12, repayments.size());
+
+        Assertions.assertEquals(856.07, repayments.get(0).getPaymentAmount(), 0.01);
+        Assertions.assertEquals(814.41, repayments.get(0).getPrincipal(), 0.01);
+        Assertions.assertEquals(41.67, repayments.get(0).getInterest(), 0.01);
+        Assertions.assertEquals(9185.59, repayments.get(0).getBalance(), 0.01);
+
+        Assertions.assertEquals(856.07, repayments.get(11).getPaymentAmount(), 0.01);
+        Assertions.assertEquals(852.52, repayments.get(11).getPrincipal(), 0.01);
+        Assertions.assertEquals(3.55, repayments.get(11).getInterest(), 0.01);
+        Assertions.assertEquals(0.00, repayments.get(11).getBalance(), 0.01);
+
+        Assertions.assertEquals(10272.84, schedule.getTotalPaymentAmount(), 0.01);
+        Assertions.assertEquals(10000.00, schedule.getTotalPrincipal(), 0.01);
+        Assertions.assertEquals(272.89, schedule.getTotalInterest(), 0.01);
+    }
+}
